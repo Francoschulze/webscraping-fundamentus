ddbab9f5d494941f6eed08ddd6de7eca095aee4c
Franco <francoschulze@gmail.com>
2022-04-30 16:14:41 -0300

    Envio padrão

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import requests
 import bs4
 import tabulate
+
+# Simulando um browser para que o servidor aceite a requisição
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+# Solicitando requisição do servidor
+resposta = requests.post('https://www.fundamentus.com.br/fii_resultado.php', headers=headers)
+
+pass
