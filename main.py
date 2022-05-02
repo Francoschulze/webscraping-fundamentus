8cec5f6c88191a63f9ff9659a886d2794f208b07
Franco <francoschulze@gmail.com>
2022-05-02 10:08:49 -0300

    Envio padrão

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 import tabulate
 
 # Simulando um browser para que o servidor aceite a requisição
@@ -8,4 +8,18 @@ headers = {'User-Agent': 'Mozilla/5.0'}
 # Solicitando requisição do servidor
 resposta = requests.post('https://www.fundamentus.com.br/fii_resultado.php', headers=headers)
 
-pass
+# Criado um novo objeto que retorna uma estrutura de dados aninhados
+soup = BeautifulSoup(resposta.text, 'html.parser')
+
+# Buscando todas as linhas da tabela de fundos imobiliários
+linhas = soup.find(id='tabelaResultado').find('tbody').find_all('tr')
+
+# Utilizando o laço for para iterar sobre as linhas encontradas
+for linha in linhas:
+
+    # Encontrando todas as linhas que possuem
+    # Os dados necessários das FII
+    dados_fundo = linha.find_all('td')
+
+    # Mostrando os dados na tela utilizando o índice
+    print(dados_fundo[0].text)
